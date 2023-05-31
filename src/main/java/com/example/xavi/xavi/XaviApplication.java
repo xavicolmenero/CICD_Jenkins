3ae4189526e8f96e57074ec261f9f347667590c9
xavicolmenero <67141230+xavicolmenero@users.noreply.github.com>
2023-05-31 09:43:20 +0200

    Add files via upload

---
+++ src/main/java/com/example/xavi/xavi/XaviApplication.java
@@ -0,0 +1,13 @@
+package com.example.xavi.xavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XaviApplication.class, args);
+	}
+
+}
